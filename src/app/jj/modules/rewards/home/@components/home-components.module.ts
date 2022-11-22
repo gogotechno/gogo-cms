c14af7602737f21cbbb358b7f294654d9026f4ee
hehehe22222 <82989264+hehehe22222@users.noreply.github.com>
2022-11-22 15:24:50 +0800

    backup

--- src/app/jj/modules/rewards/home/@components/home-components.module.ts
+++ src/app/jj/modules/rewards/home/@components/home-components.module.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { EventCardComponent } from './event-card/event-card.component';
 import { SummaryCardComponent } from './summary-card/summary-card.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule, IonicModule],
-  exports: [SummaryCardComponent],
-  declarations: [SummaryCardComponent],
+  exports: [SummaryCardComponent, EventCardComponent],
+  declarations: [SummaryCardComponent, EventCardComponent],
 })
 export class HomeComponentsModule {}
--- src/app/jj/modules/rewards/home/home.module.ts
+++ src/app/jj/modules/rewards/home/home.module.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { HomePageRoutingModule } from './home-routing.module';
-import { HomePage } from './home.page';
 import { HomeComponentsModule } from './@components/home-components.module';
+import { HomePage } from './home.page';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule, HomeComponentsModule],
+  imports: [
+     CommonModule,
+     FormsModule, 
+     IonicModule, 
+     HomePageRoutingModule, 
+     HomeComponentsModule,
+    ],
+    exports: [],
   declarations: [HomePage],
 })
 export class HomePageModule {}
