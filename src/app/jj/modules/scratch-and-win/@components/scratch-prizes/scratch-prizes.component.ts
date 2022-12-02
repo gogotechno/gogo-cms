95aac59cc5c817b34a4dc8d9f0b31095e1f03cf8
hehehe22222 <82989264+hehehe22222@users.noreply.github.com>
2022-12-02 17:51:12 +0800

    add scratch result

--- src/app/jj/modules/scratch-and-win/@components/scratch-prizes/scratch-prizes.component.ts
+++ src/app/jj/modules/scratch-and-win/@components/scratch-prizes/scratch-prizes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { EventStatus, JJScratchAndWinEvent, JJScratchAndWinEventPrize } from 'src/app/jj/typings';
 
 @Component({
   selector: 'app-scratch-prizes',
@@ -7,8 +9,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScratchPrizesComponent implements OnInit {
 
-  constructor() { }
+  event: JJScratchAndWinEvent = {
+    name: 'Scratch and Win',
+    highlight: '',
+    description: '',
+    tnc: '',
+    status: EventStatus.ACTIVE,
+    startAt: undefined,
+    endAt: undefined,
+    logo: 'https://cdn.shopify.com/app-store/listing_images/c6f8970d63d49bc8a5992dec76e3145e/banner/COG-5aX0lu8CEAE=.jpg',
+    coverImage: '',
+    backgroundImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsvA5K8aKI_jjMyyFE5cxxuubj-zRcxpXl1A&usqp=CAU',
+    scratchBackgroundImage: '',
+    scratchPlaceholderImage: '',
+    distance: '',
+  };
 
-  ngOnInit() {}
+  prizes: JJScratchAndWinEventPrize[] = [
+    {
+      name: 'Voucher',
+      description: 'Worth',
+      thumbnailImage: 'https://m8r8j2c8.stackpathcdn.com/real/imglandingscratchandwin_1.webp',
+      worth: 1000,
+      scratch_and_win_event_id: 1,
+    },
+    {
+      name: 'Voucher',
+      description: 'Worth',
+      thumbnailImage: 'https://m8r8j2c8.stackpathcdn.com/real/imglandingscratchandwin_1.webp',
+      worth: 100,
+      scratch_and_win_event_id: 2,
+    },
+    {
+      name: 'Voucher',
+      description: 'Worth',
+      thumbnailImage: 'https://m8r8j2c8.stackpathcdn.com/real/imglandingscratchandwin_1.webp',
+      worth: 200,
+      scratch_and_win_event_id: 3,
+    },
+    {
+      name: 'Voucher',
+      description: 'Worth',
+      thumbnailImage: 'https://m8r8j2c8.stackpathcdn.com/real/imglandingscratchandwin_1.webp',
+      worth: 300,
+      scratch_and_win_event_id: 4,
+    },
+    {
+      name: 'Voucher',
+      description: 'Worth',
+      thumbnailImage: 'https://m8r8j2c8.stackpathcdn.com/real/imglandingscratchandwin_1.webp',
+      worth: 50,
+      scratch_and_win_event_id: 5,
+    },
+    {
+      name: 'Voucher',
+      description: 'Worth',
+      thumbnailImage: 'https://m8r8j2c8.stackpathcdn.com/real/imglandingscratchandwin_1.webp',
+      worth: 1000,
+      scratch_and_win_event_id: 6,
+    }
+  ]
 
+  constructor(private modalCtrl: ModalController) { }
+
+  ngOnInit() { }
+
+  async OnClosing() {
+    await this.modalCtrl.dismiss();
+
+  }
 }
--- src/app/jj/modules/scratch-and-win/@components/scratch-result/scratch-result.component.ts
+++ src/app/jj/modules/scratch-and-win/@components/scratch-result/scratch-result.component.ts
@@ -17,7 +17,7 @@ export class ScratchResultComponent implements OnInit {
     status: EventStatus.ACTIVE,
     startAt: undefined,
     endAt: undefined,
-    logo: 'https://cdn.shopify.com/app-store/listing_images/c6f8970d63d49bc8a5992dec76e3145e/banner/COG-5aX0lu8CEAE=.jpg',
+    logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgXq77y_8Q3-HAjp4yDKV1DQ2vHiYu2EBZew&usqp=CAU',
     coverImage: '',
     backgroundImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsvA5K8aKI_jjMyyFE5cxxuubj-zRcxpXl1A&usqp=CAU',
     scratchBackgroundImage: '',
@@ -29,44 +29,9 @@ export class ScratchResultComponent implements OnInit {
     {
       name: 'Voucher',
       description: 'Worth',
-      thumbnailImage: 'https://m8r8j2c8.stackpathcdn.com/real/imglandingscratchandwin_1.webp',
+      thumbnailImage: 'https://image.winudf.com/v2/image1/c2NyYXRjaC53aW4uc2NyYXRjaC5jYXJkMV9zY3JlZW5fMF8xNTU5MTE1NjY4XzA1Mg/screen-0.jpg?fakeurl=1&type=.jpg',
       worth: 1000,
       scratch_and_win_event_id: 1,
-    },
-    {
-      name: 'Voucher',
-      description: 'Worth',
-      thumbnailImage: 'https://m8r8j2c8.stackpathcdn.com/real/imglandingscratchandwin_1.webp',
-      worth: 100,
-      scratch_and_win_event_id: 2,
-    },
-    {
-      name: 'Voucher',
-      description: 'Worth',
-      thumbnailImage: 'https://m8r8j2c8.stackpathcdn.com/real/imglandingscratchandwin_1.webp',
-      worth: 200,
-      scratch_and_win_event_id: 3,
-    },
-    {
-      name: 'Voucher',
-      description: 'Worth',
-      thumbnailImage: 'https://m8r8j2c8.stackpathcdn.com/real/imglandingscratchandwin_1.webp',
-      worth: 300,
-      scratch_and_win_event_id: 4,
-    },
-    {
-      name: 'Voucher',
-      description: 'Worth',
-      thumbnailImage: 'https://m8r8j2c8.stackpathcdn.com/real/imglandingscratchandwin_1.webp',
-      worth: 50,
-      scratch_and_win_event_id: 5,
-    },
-    {
-      name: 'Voucher',
-      description: 'Worth',
-      thumbnailImage: 'https://m8r8j2c8.stackpathcdn.com/real/imglandingscratchandwin_1.webp',
-      worth: 1000,
-      scratch_and_win_event_id: 6,
     }
   ]
 
@@ -76,6 +41,5 @@ export class ScratchResultComponent implements OnInit {
 
   async OnClosing() {
     await this.modalCtrl.dismiss();
-
   }
 }
