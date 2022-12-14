36846f5dc144aa6d121d191673810978b0207961
14ev5d74lz <14ev5d74lz@protonmail.com>
2022-12-14 09:15:59 +0800

    Change routing

--- src/app/jj/modules/wallets/wallets-routing.module.ts
+++ src/app/jj/modules/wallets/wallets-routing.module.ts
@@ -63,11 +63,11 @@ const routes: Routes = [
         path: 'change-pin',
         loadChildren: () => import('./change-pin/change-pin.module').then((m) => m.ChangePinPageModule),
       },
+      {
+        path: 'verify-pin',
+        loadChildren: () => import('./verify-pin/verify-pin.module').then((m) => m.VerifyPinPageModule),
+      },
     ],
-  },
-  {
-    path: 'verify-pin',
-    loadChildren: () => import('./verify-pin/verify-pin.module').then((m) => m.VerifyPinPageModule),
   },
 ];
 
