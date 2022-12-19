9567d1b215bd7ee16ad8fb71c2b0c79b1d8d1951
junhao37 <andrewwchia@gmail.com>
2022-12-19 18:43:25 +0800

    Merge branch 'lucky-draw-dev' of https://github.com/14ev5d74lz/gogo-cms into lucky-draw

--- src/app/jj/modules/membership/account/bank-account/bank-account.module.ts
+++ src/app/jj/modules/membership/account/bank-account/bank-account.module.ts
@@ -7,13 +7,17 @@ import { IonicModule } from '@ionic/angular';
 import { BankAccountPageRoutingModule } from './bank-account-routing.module';
 
 import { BankAccountPage } from './bank-account.page';
+import { SharedModule } from 'src/app/jj/shared';
+import { CmsUIModule } from 'src/app/cms-ui/cms-ui.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    BankAccountPageRoutingModule
+    BankAccountPageRoutingModule,
+    SharedModule,
+    CmsUIModule,
   ],
   declarations: [BankAccountPage]
 })
--- src/app/jj/modules/membership/account/bank-accounts/bank-accounts.module.ts
+++ src/app/jj/modules/membership/account/bank-accounts/bank-accounts.module.ts
@@ -8,12 +8,15 @@ import { BankAccountsPageRoutingModule } from './bank-accounts-routing.module';
 
 import { BankAccountsPage } from './bank-accounts.page';
 
+import { SharedModule } from 'src/app/jj/shared';
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    BankAccountsPageRoutingModule
+    BankAccountsPageRoutingModule,
+    SharedModule
   ],
   declarations: [BankAccountsPage]
 })
